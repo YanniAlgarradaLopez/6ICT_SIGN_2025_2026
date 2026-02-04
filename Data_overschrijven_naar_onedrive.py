410cffedd6d108e2cf222a21db94ba5f7866de85
YanniAlgarradaLopez <yannialgarrada@icloud.com>
2026-02-04 11:24:58 +0100

    auto update S(date '+%Y-%m-%d %H:%M:%S')

--- Data_overschrijven_naar_onedrive.py
+++ Data_overschrijven_naar_onedrive.py
@@ -19,7 +19,7 @@ while True:
     try:
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        f.open("DHT11_cloud.
+        f.open("DHT11_cloud.csv", 
         print("Temp: {:.1f} C    Humidity: {}% ".format(temperature_c, humidity))
     except RuntimeError as error:
         print(error.args[0])
