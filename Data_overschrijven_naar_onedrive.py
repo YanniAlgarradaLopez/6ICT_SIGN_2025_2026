33fd4fdc383f7fee09fd99c66f52f120b24211e0
YanniAlgarradaLopez <yannialgarrada@icloud.com>
2026-02-04 11:22:10 +0100

    auto update S(date '+%Y-%m-%d %H:%M:%S')

--- Data_overschrijven_naar_onedrive.py
+++ Data_overschrijven_naar_onedrive.py
@@ -1,3 +1,31 @@
+import time
+import board
+import adafruit_dht
 import subprocess
 
-subprocess.run(["/bin/bash", "/home/rpi/overschrijven.sh"])
+
+dhtDevice = adafruit_dht.DHT11(board.D18)
+
+huidige_tijd = time.ctime()
+print(huidige_tijd)
+
+tijd_in_seconden = time.time()
+print(tijd_in_seconden)
+
+tijdzone = time.tzname
+print(tijdzone)
+    
+while True:
+    try:
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f} C    Humidity: {}% ".format(temperature_c, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+    subprocess.run(["/bin/bash", "/home/rpi/overschrijven.sh"])
+    time.sleep(2.0)
